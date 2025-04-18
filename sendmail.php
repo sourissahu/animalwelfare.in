b2762b7c5922d5c66d8e9c66fb80a786d4c9d8e5
sourissahu <sourissahu@gmail.com>
2025-04-18 13:18:50 +0530

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,14 +1,4 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
 
-  $to = "animalwelfare4iith@gmail.com";
-  $subject = "New Contact Form Message";
-  $body = "From: $name\nEmail: $email\n\n$message";
-
-  mail($to, $subject, $body);
-  header("Location: thankyou.html");
-}
+echo "PHP is working!";
 ?>
